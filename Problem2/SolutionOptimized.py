097492e0bc64d65bc7e3425786e9250c6cecfc5b
karan kiri <karan.kiri@rebelfoods.co>
2020-03-29 23:28:38 +0530

    problem2

--- Problem2/SolutionOptimized.py
+++ Problem2/SolutionOptimized.py
@@ -1,14 +1,27 @@
 import unittest
 
 def ArrayMultiplication(list):
-  resultList = []
-  for i in range(len(list)):
-    multiplication = 1
-    for j in range(len(list)):
-      if i!=j:
-        multiplication = multiplication * list[j]
-    resultList.append(multiplication)
-  return resultList
+  multiplication = 1
+  multiplicationWithoutZero = 1
+  zeroCount = 0
+  for item in list:
+    if item == 0:
+      zeroCount = zeroCount + 1
+    else:
+      multiplicationWithoutZero = item * multiplicationWithoutZero
+    multiplication = item * multiplication
+  if zeroCount > 1:
+    return [0] * len(list)
+  elif zeroCount == 1:
+    for i in range(len(list)):
+      if(list[i] == 0):
+        list[i] = multiplicationWithoutZero
+      else:
+        list[i] = 0
+  else:
+    for i in range(len(list)):
+      list[i] = multiplication / list[i]
+  return list
 
 class Test(unittest.TestCase):
     def test_return_true_if_multiplication(self):
